d45fa86ac2b4a3c491d2155487b51e616691291a
LaNeitD18 <phanhuytien14@gmail.com>
2020-05-20 14:54:05 +0700

    Design page ThayDoiQuyDinh_Page

--- MasterSaveDemo/View/ThayDoiQuyDinh_Page.xaml.cs
+++ MasterSaveDemo/View/ThayDoiQuyDinh_Page.xaml.cs
@@ -24,5 +24,10 @@ namespace MasterSaveDemo.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

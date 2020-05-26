ecc3337a995624712ac8b22520704954ed1b92a7
lhmson <lehoangminhson123@gmail.com>
2020-05-26 12:01:16 +0700

    State after pull from Thang (trivial edit)

--- MasterSaveDemo/Model/QLSTKModel1.Designer.cs
+++ MasterSaveDemo/Model/QLSTKModel1.Designer.cs
@@ -1,4 +1,3 @@
-
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

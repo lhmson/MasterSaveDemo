f7d5f8dbbb71a64ebd17f50a4fe8d259819a50aa
thanhtrung2511 <18520179@gm.uit.edu.vn>
2020-06-12 00:07:28 +0700

    Update in BaoCaoMoDong_PrintPreview: - now user can close the Printpreview Undone: - Binding BaoCaoMoDong to Printpreview

--- MasterSaveDemo/ViewModel/BaoCaoMoDong_PrintPreview_ViewModel.cs
+++ MasterSaveDemo/ViewModel/BaoCaoMoDong_PrintPreview_ViewModel.cs
@@ -7,22 +7,32 @@ using System.Windows.Forms;
 using System.Collections.ObjectModel;
 using MasterSaveDemo.Model;
 using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MasterSaveDemo.ViewModel
 {
     class BaoCaoMoDong_PrintPreview_ViewModel: BaseViewModel
     {
         public ICommand CloseWindowCommand { get; set; }
+        private bool _IsButtonEnabled { get; set; }
+        public bool IsButtonEnabled
+        {
+            get { return _IsButtonEnabled; }
+            set
+            {
+                _IsButtonEnabled = value;
+                OnPropertyChanged("IsButtonEnabled");
+            }
+        }
         public BaoCaoMoDong_PrintPreview_ViewModel()
         {
-            CloseWindowCommand = new RelayCommand<object>((p) =>
-            {
-                return true;
-            },
-           (p) =>
-           {
-               ;
-           });
+            IsButtonEnabled = true;
+            CloseWindowCommand = new RelayCommand<object>((p) => { return p == null ? false : true; }, (p) => {
+                var ex = p as Window;
+                ex.Close();
+                
+            });
         }
        
     }
